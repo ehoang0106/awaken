1517d7e068a1c9c95f4acdb1a2000ac274b061d2
Khoa Hoang <khoahoang1698@gmail.com>
2024-04-02 13:35:04 -0700

    add smooth scroll

--- src/App.js
+++ src/App.js
@@ -4,21 +4,27 @@ import JsonData from "./data/data.json";
 import { useEffect, useState } from "react";
 import Services from "./components/Services";
 import Brands from "./components/Brands";
+import SmoothScroll from "smooth-scroll";
+
+export const scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 1100,
+    speedAsDuration: true,
+});
 
 function App() {
 
-const [landingPageData, setLandingPageData] = useState({});
-useEffect(() => {
-    setLandingPageData(JsonData);
-}, []);
+    const [landingPageData, setLandingPageData] = useState({});
+    useEffect(() => {
+        setLandingPageData(JsonData);
+    }, []);
 
     return (
         <div className="App">
             <Navbar />
-            <Header data={landingPageData.Header}/>
-            <Services data={landingPageData.Services}/>
-            <Brands data={landingPageData.About}/>
-            
+            <Header data={landingPageData.Header} />
+            <Services data={landingPageData.Services} />
+            <Brands data={landingPageData.About} />
+
         </div>
     );
 }
--- src/components/Brands.jsx
+++ src/components/Brands.jsx
@@ -1,6 +1,6 @@
 const Brands = (props) => {
     return (
-        <div id="about">
+        <div id="brands">
             <div className="container">
                 <div className="col-md-10 col-md-offset-1 section-title text-center">
                     <h2>Brands</h2>
@@ -13,7 +13,7 @@ const Brands = (props) => {
                     </div>
                     <div className="col-xs-12 col-md-4">
                         <div className="about-text">
-                            <h2>Nature Wise</h2>
+                    <h2>Nature Wise</h2>
                             <p>{props.data ? props.data.paragraph : "loading..."}</p>
                         </div>
                     </div>
