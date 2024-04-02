313160a0231386e811b0f9e33f963e17618fe7a0
Khoa Hoang <khoahoang1698@gmail.com>
2024-04-02 16:25:26 -0700

    working on brands.jsx

--- src/components/Brands.jsx
+++ src/components/Brands.jsx
@@ -7,13 +7,41 @@ const Brands = (props) => {
                 </div>
 
                 <div className="row">
-                    <div className="col-xs-12 col-md-6">
+                    <div className="col-xs-12 col-md-6 m-0 p-0">
                         {" "}
                         <img src="img/nw.jpg" className="img-responsive" alt="" />{" "}
                     </div>
-                    <div className="col-xs-12 col-md-4">
+                    <div className="col-xs-12 col-md-6 m-0 p-0">
                         <div className="about-text">
                     <h2>Nature Wise</h2>
+                            <p>{props.data ? props.data.paragraph : "loading..."}</p>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="row">
+                    <div className="col-xs-12 col-md-6 m-0 p-0">
+                        <div className="about-text">
+                            <h2>Smarter Nutrition</h2>
+                            <p>{props.data ? props.data.paragraph : "loading..."}</p>
+                        </div>                        
+                    </div>
+
+                    <div className="col-xs-12 col-md-6 m-0 p-0">
+                        {" "}
+                        <img src="img/sn.jpg" className="img-responsive" alt="" />{" "}
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-xs-12 col-md-6 m-0 p-0">
+                        {" "}
+                        <img src="img/usdc.jpg" className="img-responsive" alt="" />{" "}
+                    </div>
+                    <div className="col-xs-12 col-md-6 m-0 p-0">
+                        <div className="about-text">
+                    <h2>US Doctors' Clinical</h2>
                             <p>{props.data ? props.data.paragraph : "loading..."}</p>
                         </div>
                     </div>
